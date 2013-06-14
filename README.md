622853ce856e9a32852be5f751c4c7943d01d8e5
mrapple <tony@oc.tc>
2013-06-14 14:23:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+AdminChat
+==============
+
+Plugin that adds admin chat
+
+Permissions
+===========
+
+`rocket.rocket`
+
+Allow the user to left click a player and rocket them
---
+++ src/main/java/tc/oc/adminchat/AdminChat.java
@@ -0,0 +1,44 @@
+package tc.oc.adminchat;
+
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import tc.oc.adminchat.events.AdminChatEvent;
+
+public final class AdminChat extends JavaPlugin {
+    static String PERM_NODE = "chat.admin.";
+    static String PERM_SEND = PERM_NODE + "send";
+    static String PERM_RECIEVE = PERM_NODE + "recieve";
+    static String PREFIX = "[" + ChatColor.GOLD + "A" + ChatColor.WHITE + "] ";
+
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(new Listener() {
+            @EventHandler(priority=EventPriority.MONITOR)
+            public void broadcastAdminChat(final AdminChatEvent event) {
+                Bukkit.getServer().broadcast(event.getPrefix() + event.getPlayer() + ": " + event.getMessage(), PERM_RECIEVE);
+            }
+        }, this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(!sender.hasPermission(PERM_SEND)) {
+            sender.sendMessage(ChatColor.RED + "No permission");
+        } else if(args.length == 0) {
+            sender.sendMessage(ChatColor.RED + "Usage: /a <msg>");
+        } else {
+            AdminChatEvent event = new AdminChatEvent(sender.getName(), StringUtils.join(args, " "), PREFIX);
+            Bukkit.getPluginManager().callEvent(event);
+        }
+
+        return true;
+    }
+}
